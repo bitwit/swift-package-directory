b029c05e6eca297c76828bc5ab37327fa28a3e4c
Kyle Newsome <kyle@bitwit.ca>
2018-06-12 18:16:24 -0400

    new web structure

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -10,15 +10,35 @@ const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
 
+gulp.task('copyFonts', () => {
+	return gulp.src('./src/fonts/**/**')
+		.pipe(gulp.dest('./public/fonts'))
+});
+
+gulp.task('copyImages', () => {
+	return gulp.src('./src/images/**/**')
+		.pipe(gulp.dest('./public/images'))
+});
+
+gulp.task('copyHTML', () => {
+	return gulp.src('./src/**.html')
+		.pipe(gulp.dest('./public'))
+});
+
+gulp.task('copyJSLibs', () => {
+	return gulp.src(['./src/js/**.js', '!./src/js/main.js'])
+		.pipe(gulp.dest('./public/js'))
+});
+
 gulp.task('styles', () => {
-	return gulp.src(['./scss/bootstrap.scss', './scss/style.scss'])
+	return gulp.src(['./src/scss/bootstrap.scss', './src/scss/style.scss'])
 		.pipe(sass().on('error', sass.logError))
 		// .pipe(concat('style.css'))
 		.pipe(gulp.dest('./public/css'))
 });
 
 gulp.task('js', () => {
-	return browserify('js/app.js', {debug: true})
+	return browserify('./src/js/main.js', {debug: true})
 		.transform('babelify', {
 			sourceMaps: true,
 			presets: ['env','react']
@@ -28,7 +48,7 @@ gulp.task('js', () => {
 			message: "Error: <%= error.message %>",
 			title: 'Error in JS 💀'
 		}))
-		.pipe(source('app.js'))
+		.pipe(source('main.js'))
 		.pipe(buffer())
 		.pipe(gulp.dest('public/js'))
 		.pipe(reload({stream:true}));
@@ -42,10 +62,13 @@ gulp.task('bs', () => {
 	});
 });
 
-gulp.task('default', ['bs','js','styles'], () => {
-	gulp.watch('js/**/*.js',['js']);
-	gulp.watch('css/**/*.scss',['styles']);
+gulp.task('default', ['bs', 'build'], () => {
+	gulp.watch('./src/**.html',['copyHTML']);
+	gulp.watch('./src/js/**/*.js',['js']);
+	gulp.watch('./src/css/**/*.scss',['styles']);
+
 	gulp.watch('./public/css/style.css',reload);
+	gulp.watch('./public/*.html',reload);
 });
 
-gulp.task('build', ['js','styles']);
+gulp.task('build', ['js','styles', 'copyFonts', 'copyImages', 'copyHTML', 'copyJSLibs']);

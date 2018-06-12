eda565457a2b68a82a384e316857085178a38ca7
Kyle Newsome <kyle@bitwit.ca>
2018-06-12 17:40:40 -0400

    frontend flow in progress

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,16 +1,51 @@
 const gulp = require('gulp');
+const babel = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const notify = require('gulp-notify');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const concat = require('gulp-concat');
 
-gulp.task('default', function() {
-
-    var gulp = require('gulp');
-    var sass = require('gulp-sass');
-    
-    return gulp.src(['./scss/bootstrap.scss', './scss/style.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-    
+gulp.task('styles', () => {
+	return gulp.src(['./scss/bootstrap.scss', './scss/style.scss'])
+		.pipe(sass().on('error', sass.logError))
+		// .pipe(concat('style.css'))
+		.pipe(gulp.dest('./public/css'))
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('scss/*.scss', ['default']);
+gulp.task('js', () => {
+	return browserify('js/app.js', {debug: true})
+		.transform('babelify', {
+			sourceMaps: true,
+			presets: ['env','react']
+		})
+		.bundle()
+		.on('error',notify.onError({
+			message: "Error: <%= error.message %>",
+			title: 'Error in JS 💀'
+		}))
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('public/js'))
+		.pipe(reload({stream:true}));
 });
+
+gulp.task('bs', () => {
+	return browserSync.init({
+		server: {
+			baseDir: './public'
+		}
+	});
+});
+
+gulp.task('default', ['bs','js','styles'], () => {
+	gulp.watch('js/**/*.js',['js']);
+	gulp.watch('css/**/*.scss',['styles']);
+	gulp.watch('./public/css/style.css',reload);
+});
+
+gulp.task('build', ['js','styles']);

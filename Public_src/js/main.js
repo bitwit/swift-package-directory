99b63e415cd7a46340acc78e2e9f518d8745da95
Kyle Newsome <kyle@bitwit.ca>
2018-06-13 17:00:38 -0400

    fix api url path

--- Public_src/js/main.js
+++ Public_src/js/main.js
@@ -1,4 +1,4 @@
-const apiBaseUrl = "/";
+const apiBaseUrl = window.location.host + "/";
 const searchInput = $("#search-input");
 const performSearch = () => {
   const url = apiBaseUrl + "/search?query=";

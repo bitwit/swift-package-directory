5090af655f536be698e255934170ce60a2826975
Kyle Newsome <kyle@bitwit.ca>
2018-07-02 12:01:57 -0400

    curl issue on linux, commented out

--- Sources/SPDCore/Functions/performRequest.swift
+++ Sources/SPDCore/Functions/performRequest.swift
@@ -31,7 +31,7 @@ public func perform<T: Decodable>(request: URLRequest, transformingResponseTo re
             guard let results = try? JSONDecoder().decode(T.self, from: json) else {
                 resolver.reject(SPDError.fatal("failed to decode to \(String.init(describing: T.self))"))
                 print(try! JSONSerialization.jsonObject(with: data!, options: []))
-                print(request.cURL)
+//                print(request.cURL)
                 return
             }
             
